66a459522160ef62cb79e7a75c5481c7a8f8ad32
Manav Saspal <manavsuspal@gmail.com>
2023-03-26 22:20:27 +0530

    set up for vercel

--- localscene/settings.py
+++ localscene/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yyx7n!be-2xj0zov_*wk%&a!!u!e7)2imy^!i8-(5en1y1jvi0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [] 
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app'] 
 
 
 # Application definition
